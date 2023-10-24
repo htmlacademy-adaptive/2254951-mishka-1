9e1bbd19eddf8d2db8b38d16acd1d1f5b95070f8
Iliana Gavrilova <ilianagis98@gmail.com>
2023-10-24 19:26:41 +0300

    installed packages, added photos/icons to img

--- gulpfile.js
+++ gulpfile.js
@@ -91,8 +91,8 @@ const sprite = () => {
 
 const copy = (done) => {
   gulp.src([
-    'source/fonts/**/*.{woff2, woff}',
-    'source/*.ico',
+    'source/fonts/*.{woff2, woff}',
+    // 'source/*.ico',
     'source/manifest.webmanifest',
   ], {
     base: 'source'
